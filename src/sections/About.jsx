a0a0151c9fa284d0712c3a19016314adf043e5de
EnzoCarrilho <enzocarrilho25@gmail.com>
2026-07-18 19:15:55 -0300

    feat: implementando sobre

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,3 +1,34 @@
+import { Code2, Rocket } from "lucide-react"
+
+const highlights = [
+    {
+        icon: Code2,
+        title: 'Clean Code',
+        description: 'Foco em um código limpo, legível e de fácil manutenção, seguindo boas práticas de desenvolvimento.'
+    },
+    {
+        icon: Rocket,
+        title: 'Clean Code',
+        description: 'Foco em um código limpo, legível e de fácil manutenção, seguindo boas práticas de desenvolvimento.'
+    },
+]
+
 export const About = () => {
-    return <section></section>
+    return( 
+        <section id="about" className="py-3 relative overflow-hidden">
+            <div className="container mx-auto px-6 relative z-10">
+                <div className="grid lg:grid-cols-2 gap-16 items-center">
+                    {/* Left Column */}
+                    <div className="space-y-8">
+                        <span>Sobre</span>
+                    </div>
+
+                    <h2>
+                        Building the future,
+                        <span>one component at a time.</span>
+                    </h2>
+                </div>
+            </div>
+        </section>
+    )
 }   
